7c739945986dc673cf6c2ca2f1f552c386f2b003
ayushgitai25 <ayush25s9gautam@gmail.com>
2025-11-23 02:47:25 +0530

    Added Data tranformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTranformation, DataTransformationConfig
+
 ## To define classs variables we bascially use init, but using `dataclass` we will be directly
 ## be able to define my class variable
 @dataclass  
@@ -49,4 +51,7 @@ class DataIngestion:
 ## Testing
 if __name__ =="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTranformation()
+    data_transformation.initiate_data_transformation(train_path=train_data, test_path=test_data)
